ed281694300f76aa005fcd02abb46ced86be5e94
Lucas Rodrigues <rodrigues14set@hotmail.com>
2022-03-21 21:57:51 -0300

    Add project files.

---
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        // Validação de notificaçoes
+
+        // Validação de ModelState
+
+        // Validação da operação de negocios
+    }
+}
---
+++ src/DevIO.Api/Program.cs
@@ -0,0 +1,44 @@
+using DevIO.Api.Configuration;
+using DevIO.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//builder.Configuration
+//    .SetBasePath(builder.Environment.ContentRootPath)
+//    .AddJsonFile("appsettings.json", true, true)
+//    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+//    .AddEnvironmentVariables();
+
+// ConfigureServices
+
+builder.Services.AddDbContext<MeuDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.ResolveDependencies();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Configure
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
